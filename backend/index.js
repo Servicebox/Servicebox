d346822aba7470d1368c650d2803ad357c0beb71
Koznova Tamara <s89062960353@yandex.ru>
2023-12-04 06:15:56 +0300

    mongo добавление

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const PORT = 5000;
+const { URL } = require('./utils/constants');
 const cors = require('cors');
 const index = require('./routes/index');
 const app = express();
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', true);
+
+mongoose
+  .connect(URL)
+  .then(() => {
+    console.log('БД подключена');
+  })
+  .catch(() => {
+    console.log('Не удалось подключиться к БД');
+  });
 
 app.use(cors());
 app.use(express.json());
---
+++ backend/utils/constants.js
@@ -0,0 +1,6 @@
+const { config } = require('dotenv');
+const URL = 'mongodb://127.0.0.1:27017/serviceboxdb';
+
+module.exports = {
+    URL,
+};
