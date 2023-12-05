86ea0ec9d5bd8b2673f4fff3affa01298ccf3e61
Koznova Tamara <s89062960353@yandex.ru>
2023-12-05 15:40:34 +0300

    исправление формы

--- frontend/src/components/Form/Form.js
+++ frontend/src/components/Form/Form.js
@@ -1,26 +1,16 @@
-import React, { useEffect, useState } from 'react';
 import './Form.css';
-import СloseIcon from "../../images/x.svg"
+import CloseIcon from "../../images/x.svg";
+import { useState } from 'react';
 
 const Form = ({ toggleForm }) => {
   const [name, setName] = useState('');
   const [phone, setPhone] = useState('');
   const [description, setDescription] = useState('');
 
-  const [nameDirty, setNameDirty] = useState(false);
-  const [phoneDirty, setPhoneDirty] = useState(false);
-  const [nameError, setNameError] = useState('Имя не может быть пустым');
-  const [phoneError, setPhoneError] = useState('Телефон не может быть пустым');
+  const [nameError, setNameError] = useState('');
+  const [phoneError, setPhoneError] = useState('');
 
   const [formValid, setFormValid] = useState(false);
-
-  useEffect(() => {
-    if (phoneError || nameError) {
-      setFormValid(false);
-    } else {
-      setFormValid(true);
-    }
-  }, [phoneError, nameError]);
 
   const changeName = (e) => {
     setName(e.target.value);
@@ -30,16 +20,18 @@ const Form = ({ toggleForm }) => {
     } else {
       setNameError('');
     }
+    validateForm();
   };
 
   const changeHandlerPhone = (e) => {
     setPhone(e.target.value);
-    const re = /^\+?[78][-\(]?\d{3}\)?-?\d{3}-?\d{2}-?\d{2}$/;
+    const re = /^(\+?7|8)?[-(]?\d{3}[-)]?\d{3}[-]?\d{2}[-]?\d{2}$/;
     if (!re.test(String(e.target.value).toLowerCase())) {
       setPhoneError('Некорректный номер телефона');
     } else {
       setPhoneError('');
     }
+    validateForm();
   };
 
   const changeHandlerDescription = (e) => {
@@ -48,35 +40,17 @@ const Form = ({ toggleForm }) => {
 
   const submitData = (e) => {
     e.preventDefault();
-    fetch('http://localhost:3000/telegram', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ name, phone, description }),
-    })
-      .then((response) => response.json())
-      .then((result) => {
-        if (result.status === 'ok') {
-          alert(result.message);
-          toggleForm();
-        } else {
-          alert(result.message);
-        }
-      })
-      .catch((error) => console.error(error));
+    if (!name || !phone || nameError || phoneError) {
+      return; // Не отправляем форму если есть ошибки или поля не заполнены
+    }
+    console.log("Form submitted:", { name, phone, description });
   };
 
-  const blurHandler = (e) => {
-    switch (e.target.name) {
-      case 'name':
-        setNameDirty(true);
-        break;
-      case 'phone':
-        setPhoneDirty(true);
-        break;
-      default:
-        break;
+  const validateForm = () => {
+    if (name && phone && !nameError && !phoneError) {
+      setFormValid(true);
+    } else {
+      setFormValid(false);
     }
   };
 
@@ -86,28 +60,24 @@ const Form = ({ toggleForm }) => {
         <h2 className="form__title">
           Оставьте заявку на <span className="besplatnaya">бесплатную</span> консультацию
         </h2>
-        <form className="form">
-          {(nameDirty && nameError) && <div className="error1">{nameError}</div>}
+        <form className="form" onSubmit={submitData}>
+          {nameError && <div className="error1">{nameError}</div>}
           <label className="form__label">
             <input
               className="form__input"
               type="text"
               value={name}
-              name="name"
               onChange={changeName}
-              onBlur={blurHandler}
               placeholder="Введите ваше имя:"
             />
           </label>
-          {(phoneDirty && phoneError) && <div className="error3">{phoneError}</div>}
+          {phoneError && <div className="error3">{phoneError}</div>}
           <label className="form__label">
             <input
               className="form__input"
               onChange={changeHandlerPhone}
-              onBlur={blurHandler}
               type="text"
               value={phone}
-              name="phone"
               placeholder="Введите номер телефона:"
             />
           </label>
@@ -115,19 +85,18 @@ const Form = ({ toggleForm }) => {
             <textarea
               className="form__text"
               onChange={changeHandlerDescription}
-              name="description"
               placeholder="Опишите вашу проблему..."
               value={description}
               cols="30"
               rows="3"
             ></textarea>
           </label>
-          <button className="form-overlay__btn" disabled={!formValid} type="submit" onClick={submitData}>
+          <button className="form-overlay__btn" type="submit" disabled={!formValid}>
             Отправить форму
           </button>
         </form>
         <button className="close-button" onClick={toggleForm}>
-          <img className="close-button__img" src={СloseIcon} alt="Закрыть" />
+          <img className="close-button__img" src={CloseIcon} alt="Закрыть" />
         </button>
       </div>
     </div>
