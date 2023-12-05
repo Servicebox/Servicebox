66c60129e64f8a3720bf33257e5e59838c76c9f9
Koznova Tamara <s89062960353@yandex.ru>
2023-12-05 16:21:37 +0300

    форма отправка сообщений

--- frontend/src/components/Form/Form.js
+++ frontend/src/components/Form/Form.js
@@ -1,16 +1,25 @@
 import './Form.css';
 import CloseIcon from "../../images/x.svg";
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const Form = ({ toggleForm }) => {
   const [name, setName] = useState('');
   const [phone, setPhone] = useState('');
   const [description, setDescription] = useState('');
 
-  const [nameError, setNameError] = useState('');
-  const [phoneError, setPhoneError] = useState('');
-
+  const [nameDirty, setNameDirty] = useState(false);
+  const [phoneDirty, setPhoneDirty] = useState(false);
+  const [nameError, setNameError] = useState('Имя не может быть пустым');
+  const [phoneError, setPhoneError] = useState('Телефон не может быть пустым');
   const [formValid, setFormValid] = useState(false);
+
+  useEffect(() => {
+    if (phoneError || nameError) {
+      setFormValid(false);
+    } else {
+      setFormValid(true);
+    }
+  }, [phoneError, nameError]);
 
   const changeName = (e) => {
     setName(e.target.value);
@@ -20,18 +29,16 @@ const Form = ({ toggleForm }) => {
     } else {
       setNameError('');
     }
-    validateForm();
   };
 
   const changeHandlerPhone = (e) => {
     setPhone(e.target.value);
-    const re = /^(\+?7|8)?[-(]?\d{3}[-)]?\d{3}[-]?\d{2}[-]?\d{2}$/;
+    const re = /^\+?[78][-(]?\d{3}[-)]?\d{3}[-]?\d{2}[-]?\d{2}$/;
     if (!re.test(String(e.target.value).toLowerCase())) {
       setPhoneError('Некорректный номер телефона');
     } else {
       setPhoneError('');
     }
-    validateForm();
   };
 
   const changeHandlerDescription = (e) => {
@@ -40,17 +47,23 @@ const Form = ({ toggleForm }) => {
 
   const submitData = (e) => {
     e.preventDefault();
-    if (!name || !phone || nameError || phoneError) {
-      return; // Не отправляем форму если есть ошибки или поля не заполнены
-    }
-    console.log("Form submitted:", { name, phone, description });
+    sendMsg({ name, phone, description })
+      .then(response => {
+        // Handle the response
+      })
+      .catch(error => console.error(error));
   };
 
-  const validateForm = () => {
-    if (name && phone && !nameError && !phoneError) {
-      setFormValid(true);
-    } else {
-      setFormValid(false);
+  const blurHandler = (e) => {
+    switch (e.target.name) {
+      case 'name':
+        setNameDirty(true);
+        break;
+      case 'phone':
+        setPhoneDirty(true);
+        break;
+      default:
+        break;
     }
   };
 
@@ -61,23 +74,27 @@ const Form = ({ toggleForm }) => {
           Оставьте заявку на <span className="besplatnaya">бесплатную</span> консультацию
         </h2>
         <form className="form" onSubmit={submitData}>
-          {nameError && <div className="error1">{nameError}</div>}
+          {nameDirty && nameError && <div className="error1">{nameError}</div>}
           <label className="form__label">
             <input
               className="form__input"
               type="text"
               value={name}
+              name="name"
               onChange={changeName}
+              onBlur={blurHandler}
               placeholder="Введите ваше имя:"
             />
           </label>
-          {phoneError && <div className="error3">{phoneError}</div>}
+          {phoneDirty && phoneError && <div className="error3">{phoneError}</div>}
           <label className="form__label">
             <input
               className="form__input"
               onChange={changeHandlerPhone}
+              onBlur={blurHandler}
               type="text"
               value={phone}
+              name="phone"
               placeholder="Введите номер телефона:"
             />
           </label>
@@ -85,6 +102,7 @@ const Form = ({ toggleForm }) => {
             <textarea
               className="form__text"
               onChange={changeHandlerDescription}
+              name="description"
               placeholder="Опишите вашу проблему..."
               value={description}
               cols="30"
