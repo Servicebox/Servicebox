f74ed2222a38ea5440c3b03c66b5fb13df3a0a38
Koznova Tamara <s89062960353@yandex.ru>
2023-12-22 02:28:18 +0300

    products.js

--- frontend/src/components/Products/Products.js
+++ frontend/src/components/Products/Products.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { fetchProducts } from '../utils/ProductsApi';
+import { fetchProducts, fetchImage } from '../utils/ProductsApi';
 import "./Products.css"
 
 const Products = () => {
@@ -7,11 +7,11 @@ const Products = () => {
 
   useEffect(() => {
     const loadProducts = async () => {
-      const authId = 5948; 
-      const authKey = 'y7rd32EeTZ2xej1rtsya8vSFiMC7wCdp'; 
-      const method = 'catalog.getElementList'; 
-      const limit = 500; 
-      const page = 1; 
+      const authId = 5948;
+      const authKey = 'y7rd32EeTZ2xej1rtsya8vSFiMC7wCdp';
+      const method = 'catalog.getElementList';
+      const limit = 500;
+      const page = 1;
 
       try {
         const fetchedProducts = await fetchProducts(authId, authKey, method, limit, page);
@@ -24,6 +24,16 @@ const Products = () => {
     loadProducts();
   }, []);
 
+  const getImageUrl = async (elementId) => {
+    try {
+      const imageUrl = await fetchImage(authId, authKey, elementId);
+      return imageUrl;
+    } catch (error) {
+      console.error('Ошибка при получении изображения:', error);
+      return null;
+    }
+  };
+
   return (
     <div>
       <h2 className="catalog-list">Каталог товаров</h2>
@@ -35,6 +45,7 @@ const Products = () => {
               <li key={price.id}>{price.name} - {price.price} {price.currency}</li>
             ))}
           </ul>
+          {product.picture && <img src={getImageUrl(product.id)} alt={product.name} />}
         </div>
       ))}
     </div>
