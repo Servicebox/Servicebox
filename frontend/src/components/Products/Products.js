ad7ce69d8cdd2a47968b38c6db5746b26f19be11
Koznova Tamara <s89062960353@yandex.ru>
2023-12-22 13:06:12 +0300

    products.js асинхронный убрала апи

--- frontend/src/components/Products/Products.js
+++ frontend/src/components/Products/Products.js
@@ -1,60 +1,38 @@
-import React, { useState, useEffect } from 'react';
-import { fetchProducts, fetchImage } from '../utils/ProductsApi';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-import "./Products.css";
-
-const Products = () => {
-  const [products, setProducts] = useState([]);
-  const authId = '5948'; 
-  const authKey = 'y7rd32EeTZ2xej1rtsya8vSFiMC7wCdp';
+function Products() {
+  const [data, setData] = useState(null);
 
   useEffect(() => {
-    const loadProducts = async () => {
-      const method = 'catalog.getElementList';
-      const limit = 500;
-      const page = 1;
-
+    const fetchData = async () => {
       try {
-        const data = await fetchProducts(authId, authKey, method, limit, page);
-        if (data.status && data.status !== 1) {
-          throw new Error(data.error);
-        }
-
-        const productsWithImages = [];
-        for (const product of data.response.items) {
-          const imageUrl = await fetchImage(authId, authKey, product.id);
-          productsWithImages.push({ ...product, imageSrc: imageUrl });
-        }
-
-        setProducts(productsWithImages);
+        const response = await axios.post('https://optfm.ru/api/', {
+          auth_id: '5948',
+          auth_key: 'y7rd32EeTZ2xej1rtsya8vSFiMC7wCdp',
+          method: 'catalog.getSectionList',
+          limit: 500,
+          page: 0
+        });
+        setData(response.data);
       } catch (error) {
-        console.error('Ошибка при выполнении запроса:', error);
+        // Обработка ошибки
+        console.error('Ошибка при получении данных:', error);
       }
     };
 
-    loadProducts();
-  }, [authId, authKey]);
+    fetchData();
+  }, []); // Пустой массив зависимостей означает, что этот эффект будет запускаться только один раз при монтировании компонента
+
+  if (!data) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div>
-      <h2 className="catalog-list">Каталог товаров</h2>
-      {products.map((product) => (
-        <div key={product.id}>
-          <h3>{product.name}</h3>
-          <ul>
-            {product.prices && product.prices.map((price) => (
-              <li key={price.id}>
-                {price.name} - {price.price} {price.currency}
-              </li>
-            ))}
-          </ul>
-          {product.imageSrc && (
-            <img src={product.imageSrc} alt={product.name} />
-          )}
-        </div>
-      ))}
+    <h2> Каталог</h2>
     </div>
   );
-};
+}
 
 export default Products;
