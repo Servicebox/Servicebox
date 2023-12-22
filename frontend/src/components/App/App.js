67685a5234b99a4b801e5ec0a5cc165240dcab40
Koznova Tamara <s89062960353@yandex.ru>
2023-12-23 01:58:33 +0300

    app.js get and post

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -24,7 +24,7 @@ function App() {
   }, [isFormOpen]);
 
   useEffect(() => {
-    axios.get('https://servicebox35.ru/products.php') 
+    axios.post('https://servicebox35.ru/products.php') 
       .then(response => {
         console.log(response.data); // обработка полученных данных
       })
