67c3e2cc79d7ebf74d6dfbbed6e39b48cf09ed78
Koznova Tamara <s89062960353@yandex.ru>
2024-03-20 23:50:21 +0300

    роуты для фото

--- frontend/src/components/AdminPanel/AdminPanelRoute/AdminPanelRoute.js
+++ frontend/src/components/AdminPanel/AdminPanelRoute/AdminPanelRoute.js
@@ -1,6 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Route, Routes } from 'react-router-dom';
  import "./AdminPanelRoute.css"
+ import CreateImage from '../Image/CreateImage'; 
+ import CreateServiceForm from './CreateServiceForm';
+import DeleteService from './DeleteService';
+import UpdateService from './UpdateService';
 
 const AdminPanelRoute = () => {
   return (
@@ -8,6 +12,13 @@ const AdminPanelRoute = () => {
       <Link className='admin__link' to="/admin/create">Cоздание</Link>
       <Link className='admin__link' to="/admin/delete">Удаление</Link>
       <Link className='admin__link' to="/admin/update">Обновление</Link>
+      <Link className='admin__link' to="/admin/create-image">Загрузка Изображения</Link>
+      <Routes>
+        <Route path="create-image" element={<CreateImage />} />
+        <Route path="create" element={<CreateServiceForm />} />
+        <Route path="delete" element={<DeleteService />} />
+        <Route path="update" element={<UpdateService />} />
+      </Routes>
     </div>
   );
 };
