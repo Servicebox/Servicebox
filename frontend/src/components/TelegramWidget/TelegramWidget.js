489bcf8d0162f7b8faf3c98e173bd8c24e7bb9a7
Koznova Tamara <s89062960353@yandex.ru>
2024-04-24 21:13:18 +0300

    telegr env

--- frontend/src/components/TelegramWidget/TelegramWidget.js
+++ frontend/src/components/TelegramWidget/TelegramWidget.js
@@ -1,30 +1,38 @@
 import React, { useEffect } from "react";
+import CryptoJS from "crypto-js";
 
 import "./TelegramWidget.css"
-import { sha256, HmacSHA256 } from "crypto-js";
-
 
 const TelegramWidget = () => {
   useEffect(() => {
-    // Добавляем функцию в глобальный объект window
-    window.onTelegramAuth = (user) => {
-      const dataCheckString = `auth_date=${user.auth_date}\nfirst_name=${user.first_name}\nid=${user.id}\nusername=${user.username}`;
-      const botToken = "6875218502:AAGQyFd6PJ5HR_DtjQsu8Y6Kz0MYSJONBjM"; // токен
-      const secretKey = sha256(botToken);
-      const hmac = HmacSHA256(dataCheckString, secretKey);
-      const hash = hmac.toString();
+    const onTelegramAuth = (user) => {
+      // Отсортированные ключи для создания строки проверки
+      const dataCheckString = [
+        `auth_date=${user.auth_date}`,
+        `first_name=${user.first_name}`,
+        `id=${user.id}`,
+        `username=${user.username}`
+      ].join('\n');
 
-      if (hash === user.hash) {
-        alert(`Logged in as ${user.first_name} ${user.last_name} (${user.id}${user.username ? `, @${user.username}` : ''})`);
+      const botToken = process.env.REACT_APP_TELEGRAM_BOT_TOKEN; // Использовать переменную окружения
+      const secretKey = CryptoJS.SHA256(botToken);
+
+      const hash = CryptoJS.HmacSHA256(dataCheckString, secretKey.toString(CryptoJS.enc.Hex));
+      const hashHex = CryptoJS.enc.Hex.stringify(hash);
+
+      if (hashHex === user.hash) {
+        alert('Logged in as ' + user.first_name + ' ' + user.last_name + ' (' + user.id + (user.username ? ', @' + user.username : '') + ')');
       } else {
         alert('Data integrity check failed. Data may be compromised.');
       }
     };
 
+    window.onTelegramAuth = onTelegramAuth; // Установите глобальную функцию
+
     const script = document.createElement("script");
     script.async = true;
     script.src = "https://telegram.org/js/telegram-widget.js?22";
-    script.setAttribute("data-telegram-login", "servicB_bot"); // Имя бота
+    script.setAttribute("data-telegram-login", "servicB_bot");
     script.setAttribute("data-size", "large");
     script.setAttribute("data-onauth", "onTelegramAuth(user)");
     script.setAttribute("data-request-access", "write");
@@ -33,10 +41,11 @@ const TelegramWidget = () => {
 
     return () => {
       document.body.removeChild(script);
+      window.onTelegramAuth = undefined; // Очистите глобальную функцию
     };
   }, []);
 
   return <div className="telegram-widget-container"></div>;
 };
-//
+
 export default TelegramWidget;
