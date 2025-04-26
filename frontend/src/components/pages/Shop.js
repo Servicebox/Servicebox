8abefdfa2b99d8800623e8e6c98b50fb00564284
Koznova Tamara <s89062960353@yandex.ru>
2025-04-26 21:51:36 +0300

    ошибка по Shop

--- frontend/src/components/pages/Shop.js
+++ frontend/src/components/pages/Shop.js
@@ -1,24 +1,22 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
-import { BrowserRouter, Routes,Router,Route , useNavigate} from 'react-router-dom';
- import NavBar from '../NavBar';
+import { useNavigate } from 'react-router-dom';
+
 //import Hero from '../Hero/Hero';
 
 //import Offers from '../Offers/Offers';
-import NewCollections from '../NewCollections/NewCollections';
-import Popular from '../Popular/Popular';
-import Item from '../Item/Item';
+
 import "./Shop.css"
 
 
- const Shop = () => {
-    const navigate = useNavigate()
- 
-    return (
-        <div className=''>
- <a href="https://ru.servicebox.shop/" className="link"  />
-   </div>
-    );
+const Shop = () => {
+  const navigate = useNavigate()
+
+  return (
+    <div className=''>
+      <a href="https://ru.servicebox.shop/" className="link" />
+    </div>
+  );
 }
 
 export default Shop;
